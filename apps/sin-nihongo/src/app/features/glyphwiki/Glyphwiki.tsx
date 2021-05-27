174109cdeb8dc209cbf41e6f3acae6bda1ef23f2
nagoya313 <nagoya313@gmail.com>
2021-05-28 05:44:35 +0900

    canvas設置

--- apps/sin-nihongo/src/app/features/glyphwiki/Glyphwiki.tsx
+++ apps/sin-nihongo/src/app/features/glyphwiki/Glyphwiki.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import Avatar from '@material-ui/core/Avatar';
+import Box from '@material-ui/core/Box';
 import CardHeader from '@material-ui/core/CardHeader';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
+import { withTheme } from '@material-ui/core/styles';
 import { GLYPHWIKI_QUERY_PARAMS_MATCHER, KageRecursionData } from '@sin-nihongo/api-interfaces';
 import { NewTabLink } from '../../components/NewTabLink';
 import { SearchForm } from '../../components/SearchForm';
@@ -14,6 +16,18 @@ import { GlyphwikiData } from './GlyphwikiData';
 
 const CardAvatar = styled(Avatar)`
   background-color: ${red[500]};
+`;
+
+const CanvasBox = withTheme(styled(Box)`
+  margin: ${(props) => props.theme.spacing(1)}px;
+  position: relative;
+  width: 200px;
+  height: 200px;
+`);
+
+const Canvas = styled.canvas`
+  border: 1px solid gray;
+  background-color: white;
 `;
 
 const splitData = (data: string | undefined) => {
@@ -30,6 +44,7 @@ const splitData = (data: string | undefined) => {
 export const Glyphwiki = () => {
   const [searchWord, setSearchWord] = useState('');
   const [kageData, setKageData] = useState<KageRecursionData>();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const validation = (word: string) => word.match(GLYPHWIKI_QUERY_PARAMS_MATCHER) !== null || word === '';
 
   return (
@@ -57,7 +72,10 @@ export const Glyphwiki = () => {
               errorMessage="検索項目が不正です"
             />
           </GridListTile>
-          <GridListTile>
+          <GridListTile style={{ height: 'auto' }}>
+            <CanvasBox>
+              <Canvas ref={canvasRef} width="200px" height="200px" />
+            </CanvasBox>
             <GridListTileBar
               title={kageData?.name}
               subtitle={splitData(kageData?.needGlyphs[0]?.data)}

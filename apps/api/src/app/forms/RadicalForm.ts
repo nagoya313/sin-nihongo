4331973f2f707311aaae4de5c8579c22cb9915c8
nagoya313 <nagoya313@gmail.com>
2021-06-01 03:19:26 +0900

    部首検索フォーム追加

--- apps/api/src/app/forms/RadicalForm.ts
+++ apps/api/src/app/forms/RadicalForm.ts
@@ -1,3 +1,12 @@
-import { GenericsSearchQueryParams, integerableQueryParams } from '../libs/genericsSearchQueryParams';
+import { IsInt, Min, IsOptional, Matches } from 'class-validator';
 
-export class RadicalsQueryParams extends integerableQueryParams(GenericsSearchQueryParams) {}
+export class RadicalsQueryParams {
+  @IsOptional()
+  @Matches(/^(?!.*[ぢづゐゑを])[\u3040-\u3093ー]*$/, { message: `"$value"わ検索不可能なよみがなです。` })
+  nameLike: string;
+
+  @IsOptional()
+  @IsInt({ message: 'わ整数で入力してください。' })
+  @Min(1, { message: 'わ$constraint1以上で入力してください。' })
+  numberOfStrokes: number;
+}

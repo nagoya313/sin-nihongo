a8bd711ac9e132407e108a7585f87b9685313d73
nagoya313 <nagoya313@gmail.com>
2021-05-31 14:18:15 +0900

    migration周り修正

--- db/migrations/1622437946341-CreateRadicals.js
+++ db/migrations/1622437946341-CreateRadicals.js
@@ -1,7 +1,7 @@
 const { MigrationInterface, QueryRunner } = require("typeorm");
 
-module.exports = class Migration1622305834238 {
-    name = 'Migration1622305834238'
+module.exports = class CreateRadicals1622437946341 {
+    name = 'CreateRadicals1622437946341'
 
     async up(queryRunner) {
         await queryRunner.query(`CREATE TABLE "radicals" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "id" integer NOT NULL, "number_of_strokes" integer NOT NULL, "names" character varying array NOT NULL, CONSTRAINT "PK_0a50786fef2a9e30c0474c3ee9f" PRIMARY KEY ("id"))`);

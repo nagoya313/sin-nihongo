c6e64ad5a81c3bb5589a139547aed4693b6100b8
nagoya313 <nagoya313@gmail.com>
2021-06-01 21:09:37 +0900

    漢字response調整

--- apps/api/src/app/config/db.ts
+++ apps/api/src/app/config/db.ts
@@ -1,13 +1,12 @@
 import 'reflect-metadata';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { Kanji } from '../entities/Kanji';
-import { JisLevel } from '../entities/JisLevel';
 import { Radical } from '../entities/Radical';
 
 export const dbConfig = () => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const options: PostgresConnectionOptions = require('../../../../../ormconfig');
   return Object.assign(options, {
-    entities: [Kanji, JisLevel, Radical],
+    entities: [Kanji, Radical],
   });
 };

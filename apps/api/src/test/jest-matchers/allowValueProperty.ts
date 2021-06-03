128035fb60f04cedb1ac1fbf0c092ed71e4bab44
nagoya313 <nagoya313@gmail.com>
2021-06-04 02:06:45 +0900

    不要なimport除去

--- apps/api/src/test/jest-matchers/allowValueProperty.ts
+++ apps/api/src/test/jest-matchers/allowValueProperty.ts
@@ -1,5 +1,4 @@
 import { validate } from 'class-validator';
-import { NotImplementedError } from './NotImplementedError';
 
 interface Target {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any

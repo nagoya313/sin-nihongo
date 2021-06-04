c3f18854f99bd301877446aae24e820078b583b8
nagoya313 <nagoya313@gmail.com>
2021-06-05 06:33:07 +0900

    format

--- db/migrations/1622833554583-ChangeTypeIdToKanjis.js
+++ db/migrations/1622833554583-ChangeTypeIdToKanjis.js
@@ -1,15 +1,15 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
+const { MigrationInterface, QueryRunner } = require('typeorm');
 
 module.exports = class ChangeTypeIdToKanjis1622833554583 {
-    name = 'ChangeTypeIdToKanjis1622833554583'
+  name = 'ChangeTypeIdToKanjis1622833554583';
 
-    async up(queryRunner) {
-        await queryRunner.query(`CREATE SEQUENCE "kanjis_id_seq" OWNED BY "kanjis"."id"`);
-        await queryRunner.query(`ALTER TABLE "kanjis" ALTER COLUMN "id" SET DEFAULT nextval('kanjis_id_seq')`);
-    }
+  async up(queryRunner) {
+    await queryRunner.query(`CREATE SEQUENCE "kanjis_id_seq" OWNED BY "kanjis"."id"`);
+    await queryRunner.query(`ALTER TABLE "kanjis" ALTER COLUMN "id" SET DEFAULT nextval('kanjis_id_seq')`);
+  }
 
-    async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "kanjis" ALTER COLUMN "id" DROP DEFAULT`);
-        await queryRunner.query(`DROP SEQUENCE "kanjis_id_seq"`);
-    }
-}
+  async down(queryRunner) {
+    await queryRunner.query(`ALTER TABLE "kanjis" ALTER COLUMN "id" DROP DEFAULT`);
+    await queryRunner.query(`DROP SEQUENCE "kanjis_id_seq"`);
+  }
+};

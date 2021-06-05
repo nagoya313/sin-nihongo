66ff0a3ef36578c400c3cc78448ebae628f67f65
nagoya313 <nagoya313@gmail.com>
2021-06-05 09:29:08 +0900

    グリフ表示追加

--- apps/sin-nihongo/src/app/features/glyphs/Glyphs.tsx
+++ apps/sin-nihongo/src/app/features/glyphs/Glyphs.tsx
@@ -4,9 +4,11 @@ import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
+import { Buhin } from '@kurgm/kage-engine';
 import { Glyphs as ApiGlyphs, Glyph } from '@sin-nihongo/api-interfaces';
 import { CardAvatar } from '../../components/CardAvatar';
 import { ErrorTypography } from '../../components/ErrorTypography';
+import { GlyphCanvas } from '../../components/GlyphCanvas';
 import { SearchTextField } from '../../components/SearchTextField';
 import { Table } from '../../components/Table';
 import { useAxiosGet } from '../../libs/axios';
@@ -25,6 +27,7 @@ export const Glyphs = () => {
   const [searchName, setSearchName] = useState('');
   const [pageNumber, setPageNumber] = useState(1);
   const [glyphs, setGlyphs] = useState<Glyph[]>();
+  const [buhin, setBuhin] = useState(new Buhin());
   const [{ data, loading, error }, refetch] = useAxiosGet<ApiGlyphs>('api/v1/glyphs');
 
   const onPageChange = (page: number) => {
@@ -52,6 +55,17 @@ export const Glyphs = () => {
         setGlyphs(undefined);
       } else if (typeof data !== 'undefined') {
         setGlyphs(data.items);
+        const b = new Buhin();
+        if (data) {
+          data.items.forEach((glyph) => {
+            b.push(glyph.name, glyph.data);
+          });
+          data.includeGlyphs?.forEach((glyph) => {
+            b.push(glyph.name, glyph.data);
+          });
+        }
+        setBuhin(b);
+        console.log(b);
       }
     }
   }, [data, loading, error]);
@@ -60,7 +74,7 @@ export const Glyphs = () => {
   const rows = glyphs?.map((glyph): { [key in Fields | 'key']: any } => ({
     key: `glyph_${glyph.id}`,
     name: glyph.name,
-    glyph: '',
+    glyph: <GlyphCanvas buhin={buhin} name={glyph.name} />,
     data: splitData(glyph.data),
     show: '',
   }));

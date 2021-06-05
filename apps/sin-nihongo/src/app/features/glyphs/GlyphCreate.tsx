a3f6241a4a56869b64969538d29caea564515dcd
nagoya313 <nagoya313@gmail.com>
2021-06-05 16:27:05 +0900

    preview試作

--- apps/sin-nihongo/src/app/features/glyphs/GlyphCreate.tsx
+++ apps/sin-nihongo/src/app/features/glyphs/GlyphCreate.tsx
@@ -3,11 +3,14 @@ import { useForm, Controller } from 'react-hook-form';
 import { classValidatorResolver } from '@hookform/resolvers/class-validator';
 import { ErrorMessage } from '@hookform/error-message';
 import styled from 'styled-components';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
+import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
+import BrushIcon from '@material-ui/icons/Brush';
 import { Buhin } from '@kurgm/kage-engine';
 import { CreateGlyph } from '@sin-nihongo/api-interfaces';
 import { CardAvatar } from '../../components/CardAvatar';
@@ -31,11 +34,19 @@ export const GlyphCreate: React.FC = () => {
     register,
     control,
     handleSubmit,
+    getValues,
     formState: { errors },
   } = useForm<CreateGlyph>({ mode: 'onBlur', resolver });
   const [buhin, setBuhin] = useState(new Buhin());
 
   const onSubmit = (data) => alert(JSON.stringify(data));
+
+  const onDraw = () => {
+    const data = getValues('data');
+    const b = new Buhin();
+    b.push('preview', data);
+    setBuhin(b);
+  };
 
   return (
     <Card>
@@ -45,7 +56,12 @@ export const GlyphCreate: React.FC = () => {
         titleTypographyProps={{ variant: 'h4' }}
       />
       <CardContent>
-        <GlyphCanvas buhin={buhin} name="preview" />
+        <Box display="flex" alignItems="flex-end">
+          <GlyphCanvas buhin={buhin} name="preview" />
+          <IconButton onClick={onDraw}>
+            <BrushIcon />
+          </IconButton>
+        </Box>
         <Form onSubmit={handleSubmit(onSubmit)}>
           <div style={{ width: 400, display: 'flex', flexDirection: 'column' }}>
             <Controller

47ae64e88a9e68b2a7b36e9e26efcdd24740d682
nagoya313 <nagoya313@gmail.com>
2021-06-07 03:14:55 +0900

    グリフウィキからコピー作成追加

--- apps/sin-nihongo/src/app/features/glyphwiki/GlyphwikiContent.tsx
+++ apps/sin-nihongo/src/app/features/glyphwiki/GlyphwikiContent.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import BuildIcon from '@material-ui/icons/Build';
 import ListItemText from '@material-ui/core/ListItemText';
 import { Buhin } from '@kurgm/kage-engine';
+import { GlyphForm, GlyphParams, Message } from '@sin-nihongo/api-interfaces';
 import { ClipBoard } from '../../components/ClipBoard';
 import { GlyphCanvas } from '../../components/GlyphCanvas';
+import { IconButton } from '../../components/IconButton';
 import { ListItemIcon } from '../../components/ListItemIcon';
+import { NoticeDispatchContext } from '../notice/Notice';
+import { getAccessTokenOptions } from '../../utils/auth0';
+import { errorMessage, fetchWithTokenAndData, useLazyAxiosPost } from '../../utils/axios';
 import { splitData } from '../../utils/kageData';
 
 type Props = {
@@ -19,6 +26,25 @@ type Props = {
 };
 
 export const GlyphwikiContent: React.FC<Props> = ({ isAdmin, name, data, buhin }) => {
+  const { getAccessTokenSilently } = useAuth0();
+  const [{ data: postData, error }, execute] = useLazyAxiosPost<Message>('api/v1/glyphs');
+  const noticeDispatch = useContext(NoticeDispatchContext);
+
+  const onBuild = async () => {
+    if (name && data) {
+      const token = await getAccessTokenSilently(getAccessTokenOptions);
+      fetchWithTokenAndData(execute, token, new GlyphParams({ name: name, data: data }));
+    }
+  };
+
+  useEffect(() => {
+    error && noticeDispatch({ type: 'open', message: errorMessage(error), severity: 'error' });
+  }, [error, noticeDispatch]);
+
+  useEffect(() => {
+    postData && noticeDispatch({ type: 'open', message: postData.message, severity: 'success' });
+  }, [postData, noticeDispatch]);
+
   return (
     <Box display="flex" p={1}>
       <GlyphCanvas buhin={buhin} name={name} />
@@ -35,6 +61,7 @@ export const GlyphwikiContent: React.FC<Props> = ({ isAdmin, name, data, buhin }
             <ListItemText>{name}</ListItemText>
             {isAdmin && (
               <ListItemSecondaryAction>
+                <ListItemIcon icon={<IconButton onClick={onBuild} icon={<BuildIcon />} />} />
                 <ListItemIcon icon={<ClipBoard data={name} title="複製了" />} />
               </ListItemSecondaryAction>
             )}

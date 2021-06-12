0eff3ec8373f9850b13b60ecc779cdadc311a3d9
nagoya313 <nagoya313@gmail.com>
2021-06-12 11:30:51 +0900

    調整

--- apps/sin-nihongo/src/app/components/NewTabLink.tsx
+++ apps/sin-nihongo/src/app/components/NewTabLink.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
 
-type Props = {
-  url: string;
-  text: string;
-};
+type Props = { url: string; text: string };
 
 export const NewTabLink: React.FC<Props> = ({ url, text }) => (
   <Link href={url} target="_blank" rel="noopener">

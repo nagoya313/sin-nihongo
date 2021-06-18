628dae083a7ced14f09d5d602cfaecf4894ae9b7
nagoya313 <nagoya313@gmail.com>
2021-06-18 22:43:43 +0900

    複雑性ガード追加

--- apps/api/src/app/config/apollo.ts
+++ apps/api/src/app/config/apollo.ts
@@ -2,8 +2,11 @@ import { Express } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import * as joiful from 'joiful';
 import { buildSchema, registerEnumType } from 'type-graphql';
+import { fieldExtensionsEstimator, getComplexity, simpleEstimator } from 'graphql-query-complexity';
 import { ApolloServerLoaderPlugin } from 'type-graphql-dataloader';
 import { OrderDirection } from '@sin-nihongo/graphql-interfaces';
+
+const LIMIT_COMPLEXITY = 1000;
 
 export const initApolloServer = async (app: Express) => {
   const resolvers = ['apps/api/src/app/resolvers/*.ts'] as const;
@@ -27,7 +30,28 @@ export const initApolloServer = async (app: Express) => {
     schema,
     introspection: true,
     playground: true,
-    plugins: [ApolloServerLoaderPlugin()],
+    plugins: [
+      ApolloServerLoaderPlugin(),
+      {
+        requestDidStart: () => ({
+          didResolveOperation({ request, document }) {
+            const complexity = getComplexity({
+              schema,
+              operationName: request.operationName,
+              query: document,
+              variables: request.variables,
+              estimators: [fieldExtensionsEstimator(), simpleEstimator({ defaultComplexity: 1 })],
+            });
+            if (complexity > LIMIT_COMPLEXITY) {
+              throw new Error(
+                `Sorry, too complicated query! ${complexity} is over ${LIMIT_COMPLEXITY} that is the max allowed complexity.`
+              );
+            }
+            console.log('Used query complexity points:', complexity);
+          },
+        }),
+      },
+    ],
   });
 
   server.applyMiddleware({ app });
--- apps/api/src/app/resolvers/GlyphwikiRespolver.ts
+++ apps/api/src/app/resolvers/GlyphwikiRespolver.ts
@@ -5,17 +5,17 @@ import { drawNecessaryGlyphs, includeGlyphs } from '../libs/glyph';
 
 @Resolver(() => GlyphwikiGlyph)
 export class GlyphqikiResolver {
-  @Query(() => GlyphwikiGlyph, { description: 'グリフウィキからグリフお取得する' })
+  @Query(() => GlyphwikiGlyph, { description: 'グリフウィキからグリフお取得する', complexity: 10 })
   glyphwiki(@Args() { name }: GetGlyphwikiGlyphArgs) {
     return Glyphwiki.getData(name);
   }
 
-  @FieldResolver(() => [GlyphwikiGlyph], { description: '含むグリフ' })
+  @FieldResolver(() => [GlyphwikiGlyph], { description: '含むグリフ', complexity: 50 })
   includeGlyphs(@Root() glyph: GlyphwikiGlyph) {
     return includeGlyphs(glyph, Glyphwiki.getData);
   }
 
-  @FieldResolver(() => [GlyphwikiGlyph], { description: '描画に必要なグリフ' })
+  @FieldResolver(() => [GlyphwikiGlyph], { description: '描画に必要なグリフ', complexity: 100 })
   drawNecessaryGlyphs(@Root() glyph: GlyphwikiGlyph) {
     return drawNecessaryGlyphs(glyph, Glyphwiki.getData);
   }

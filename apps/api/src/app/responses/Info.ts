332ce59e287f172654d6ecdf40383dff69679e9d
nagoya313 <nagoya313@gmail.com>
2021-06-18 22:30:46 +0900

    不要ファイル除去

--- apps/api/src/app/responses/Info.ts
+++
@@ -1,7 +0,0 @@
-import { Field, ObjectType } from 'type-graphql';
-
-@ObjectType({ description: 'サイト情報' })
-export class Info {
-  @Field({ description: 'グリフウィキからグリフお取得可能かどうか' })
-  readonly glyphwikiAccessible!: boolean;
-}

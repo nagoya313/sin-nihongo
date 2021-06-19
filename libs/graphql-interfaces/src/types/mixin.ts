889faea68d76e42b8e6137fea81c3834d3a19a94
nagoya313 <nagoya313@gmail.com>
2021-06-19 10:46:57 +0900

    不要ファイル除去

--- libs/graphql-interfaces/src/types/mixin.ts
+++
@@ -1,9 +0,0 @@
-
-type Constructor = abstract new (...args: any[]) => any;
-
-function Timestamped<TBase extends Constructor>(Base: TBase) {
-  abstract class Hoge extends Base {
-    timestamp = Date.now();
-  };
-  return Hoge
-}

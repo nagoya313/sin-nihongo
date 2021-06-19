c2f7555b4aa5b175b301836c086c3a4dbe28040e
nagoya313 <nagoya313@gmail.com>
2021-06-19 13:51:44 +0900

    id型つけ

--- apps/api/src/app/entities/Node.ts
+++ apps/api/src/app/entities/Node.ts
@@ -1,7 +1,10 @@
+import { ObjectID } from 'typeorm';
 import { AbstractClassType } from '../utils/ClassType';
 
 export const NodedEntity = <TBase extends AbstractClassType>(Base: TBase) => {
   abstract class NodeEntity extends Base {
+    abstract readonly id?: number | string | ObjectID;
+
     get encodedId() {
       return Buffer.from(`${Base.name}:${this.id}`).toString('base64');
     }

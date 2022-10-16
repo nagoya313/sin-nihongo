e4cb35166d21f286033eb83a1e4f07d77c365a44
nagoya313 <nagoya313@gmail.com>
2022-10-16 13:25:04 +0900

    canvasをclient side only化

--- app/components/GlyphCanvasSuspense.tsx
+++ app/components/GlyphCanvasSuspense.tsx
@@ -1,6 +1,7 @@
 import { Skeleton } from '@chakra-ui/react';
 import { type Buhin } from '@kurgm/kage-engine';
 import { lazy, Suspense } from 'react';
+import { ClientOnly } from 'remix-utils';
 
 const GlyphCanvas = lazy(() => import('./GlyphCanvas'));
 
@@ -10,9 +11,13 @@ type GlyphCanvasSuspenseProps = {
 };
 
 const GlyphCanvasSuspense = ({ name, buhin }: GlyphCanvasSuspenseProps) => (
-  <Suspense fallback={<Skeleton w="200px" h="200px" />}>
-    <GlyphCanvas name={name} buhin={buhin} />
-  </Suspense>
+  <ClientOnly fallback={<Skeleton w="200px" h="200px" />}>
+    {() => (
+      <Suspense fallback={<Skeleton w="200px" h="200px" />}>
+        <GlyphCanvas name={name} buhin={buhin} />
+      </Suspense>
+    )}
+  </ClientOnly>
 );
 
 export default GlyphCanvasSuspense;

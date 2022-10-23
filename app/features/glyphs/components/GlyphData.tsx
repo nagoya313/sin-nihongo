151cd0c9efa769eee5e5ee0413fa1da5634deb85
nagoya313 <nagoya313@gmail.com>
2022-10-23 23:33:35 +0900

    微修正

--- app/features/glyphs/components/GlyphData.tsx
+++ app/features/glyphs/components/GlyphData.tsx
@@ -1,6 +1,5 @@
-import { Box, HStack, IconButton, VStack } from '@chakra-ui/react';
-import { useActionData } from '@remix-run/react';
-import { useEffect, useState } from 'react';
+import { Box, HStack, IconButton, VStack, useUpdateEffect } from '@chakra-ui/react';
+import { useState } from 'react';
 import { MdOutlineEdit, MdOutlineInfo } from 'react-icons/md';
 import { $path } from 'remix-routes';
 import { ValidatedForm } from 'remix-validated-form';
@@ -14,7 +13,7 @@ import GlyphCanvas from '~/features/kage/components/GlyphCanvas';
 import { toDisplayKageData } from '~/features/kage/decorators';
 import { getGlyphCanvasProps } from '~/features/kage/models/kageData';
 import { type DrawableGlyph } from '~/features/kage/models/kageData';
-import { type action, type loader } from '~/routes/glyphs/$name';
+import { type loader } from '~/routes/glyphs/$name';
 import { type LoaderData } from '~/utils/types';
 import { glyphUpdateParams } from '../validators';
 import KageTextArea from './KageTextArea';
@@ -27,7 +26,6 @@ const GlyphData = ({ glyph }: GlyphResultProps) => {
   const [isEditMode, setIsEditMode] = useState(false);
   const [preview, setPreview] = useState<DrawableGlyph>(glyph);
   const [defaultData, setDefaultData] = useState(glyph.data ?? '');
-  const updated = useActionData<typeof action>();
   const toggle = () => {
     setIsEditMode((prev) => {
       if (prev) {
@@ -37,13 +35,11 @@ const GlyphData = ({ glyph }: GlyphResultProps) => {
     });
   };
 
-  useEffect(() => {
-    if (updated != null && 'glyph' in updated) {
-      setPreview(updated.glyph);
-      setDefaultData(updated.glyph.data);
-      setIsEditMode(false);
-    }
-  }, [updated]);
+  useUpdateEffect(() => {
+    setPreview(glyph);
+    setDefaultData(glyph.data);
+    setIsEditMode(false);
+  }, [glyph]);
 
   return (
     <HStack w="full">

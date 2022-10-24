abeb2af33884ea8d94e260f7fa7aac63d3ef366f
nagoya313 <nagoya313@gmail.com>
2022-10-24 18:10:50 +0900

    ボタンリンク修正

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -1,14 +1,10 @@
 import { IconButton, type IconButtonProps } from '@chakra-ui/react';
-import { useNavigate } from '@remix-run/react';
+import { Link } from '@remix-run/react';
 
 type LinkButtonProps = {
   to: string;
 } & Omit<IconButtonProps, 'onClick'>;
 
-const LinkButton = ({ to, ...props }: LinkButtonProps) => {
-  const navigate = useNavigate();
-
-  return <IconButton {...props} onClick={() => navigate(to)} />;
-};
+const LinkButton = (props: LinkButtonProps) => <IconButton as={Link} {...props} />;
 
 export default LinkButton;
